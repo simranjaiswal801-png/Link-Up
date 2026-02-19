cecc74cd66c680dbf6ff3a2867284ed5fa8e9480
simran jaiswal <simranjaiswal801@gmail.com>
2026-02-19 15:41:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", socket => {
+
+  socket.on("join-room", roomId => {
+    socket.join(roomId);
+
+    socket.to(roomId).emit("user-joined", socket.id);
+
+    socket.on("signal", data => {
+      io.to(data.to).emit("signal", {
+        from: socket.id,
+        signal: data.signal
+      });
+    });
+
+    socket.on("message", msg=>{
+      io.to(roomId).emit("message", msg);
+    });
+
+    socket.on("disconnect", ()=>{
+      socket.to(roomId).emit("user-left", socket.id);
+    });
+
+  });
+
+});
+
+server.listen(3000, ()=>console.log("Server running on 3000"));
